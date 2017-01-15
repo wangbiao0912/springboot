a9005a5ef360e3969d3d56115f74845da77fe294
wangbiaoJava <229492022@qq.com>
2017-01-15 23:30:06 +0800

    spring boot 更改重启规则

--- src/main/java/com/spring/DemoApplication.java
+++ src/main/java/com/spring/DemoApplication.java
@@ -16,6 +16,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     public static void main(String[] args) {
         log.info("DemoApplication is starting...");
+        //禁用改代码就重启
+        /**
+         * 如果不想使用重启特性，你可以通过 spring.devtools.restart.enabled 属性
+         来禁用它，通常情况下可以在 application.properties 文件中设置（ 依旧会初
+         始化重启类加载器，但它不会监控文件变化） 。
+         如果需要彻底禁用重启支持，比如，不能跟某个特殊库一块工作，
+         */
+        System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
